a880b5b83710f068eea3c7d9df5b2754137f91fc
Russell Ramey <doublercreative@gmail.com>
2017-04-17 10:51:34 -0500

    Included styles in WP_Head

--- wp-modules.php
+++ wp-modules.php
@@ -7,3 +7,17 @@
 * Author: Russell Ramey
 * Author URI: http://russellramey.me/
 */
+
+
+/************************************************************************************
+*** Module Styles
+	Load front end css for displaying the content modules.
+************************************************************************************/
+// Wordpress action hook
+add_action( 'wp_enqueue_scripts', 'load_module_css' );
+function load_module_css() {
+	// Plugin path
+    $plugin_path = plugin_dir_url( __FILE__ );
+    // Load styles to wp_head
+    wp_enqueue_style( 'modules', $plugin_path . 'css/modules-style.css' );
+}
