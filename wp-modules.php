adaa6a14957aef2d0f61c4bd37299796177f21d1
Russell Ramey <doublercreative@gmail.com>
2017-04-17 10:40:36 -0500

    Initial commit Plugin name and attributes

---
+++ wp-modules.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: WP Content Modules
+* Plugin URI: http://russellramey.me/wordpress/wp-content-modules
+* Description: Build pages modularly. Create simple, repeateable, blocks of content that can be used anywhere with the generated [module] shortcode.
+* Version: 1.0
+* Author: Russell Ramey
+* Author URI: http://russellramey.me/
+*/
