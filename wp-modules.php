073dfd1a986d41441421c02e6614955338166320
Russell Ramey <doublercreative@gmail.com>
2017-05-05 13:32:11 -0500

    Cleaned up codebase

--- wp-modules.php
+++ wp-modules.php
@@ -33,7 +33,6 @@ function load_module_admin_css() {
     wp_enqueue_script( 'wp-color-picker');
 }
 
-
 /************************************************************************************
 *** Module (Content Module)
 	Post type for 'Module'. Create repeatable code blocks to use as shortcodes
@@ -109,216 +108,6 @@ function add_featured_image_instruction( $content ) {
 	- REQUIRES CMB2 Metabox library
 	  @link https://github.com/CMB2/CMB2
 ************************************************************************************/
-// Check for CMB2 library
-// If CMB2 class doesn't exist, include it
-/*if( !class_exists('CMB2') ){
-    require_once( dirname(__FILE__)."/lib/cmb/init.php" );
-}
-
-// Module setup metabox
-add_action( 'cmb2_init', 'module_metabox_setup' );
-function module_metabox_setup() {
-	// Start with an underscore to hide fields from custom fields list
-	$prefix = '_cmb_';
-	$module_setup = new_cmb2_box( array(
-		'id'            => $prefix . 'module_setup',
-		'title'         => __( 'Module Setup', 'cmb2' ),
-		'object_types'  => array( 'module'), // Post type
-	));
-
-	// Module Width Selector
-	$module_setup->add_field( array(
-	    'name'             => 'Module Outer Width',
-	    'desc'             => 'Select the width of the entire module<br />- Auto (Module will flow inline with max width of parent container)<br />- Full Width (Module will fill width of viewport, background and all)',
-	    'id'               => $prefix . 'module_width',
-	    'type'             => 'select',
-	    'show_option_none' => false,
-	    'default'          => 'auto',
-	    'options'          => array(
-	        'wp-module--auto' => __( 'Auto', 'cmb2' ),
-	        'wp-module--full'   => __( 'Full Width', 'cmb2' ),
-	    ),
-	));
-	// Module Width Selector
-	$module_setup->add_field( array(
-	    'name'             => 'Module Content Width',
-	    'desc'             => 'Option to constrain or expand the width of the inner content beyond the default container (default content width is 1170px)
-                               <br />- Auto (Module content will fill same width as "Module Outer Width" above)
-                               <br />- Small (Module content max width of 768px)
-                               <br />- Medium (Module content max width of 960px)
-                               <br />- Large (Module content max width of 1280px)
-                               <br />- X Large (Module content max width of 1440px)',
-	    'id'               => $prefix . 'module_content_width',
-	    'type'             => 'select',
-	    'show_option_none' => false,
-	    'default'          => 'auto',
-        'options'          => array(
-	        'auto' => __( 'Auto', 'cmb2' ),
-	        'small'   => __( 'Small (768px)', 'cmb2' ),
-			'medium'   => __( 'Medium (960px)', 'cmb2' ),
-			'large'   => __( 'Large (1280px)', 'cmb2' ),
-			'xlarge'   => __( 'X Large (1440px)', 'cmb2' ),
-	    ),
-	));
-	// Module Height Selector
-	$module_setup->add_field( array(
-	    'name'             => 'Module Padding',
-        'desc'             => 'Select the padding for the <b>top</b> and <b>bottom</b> of the module content.',
-	    'id'               => $prefix . 'module_height',
-	    'type'             => 'select',
-	    'show_option_none' => false,
-	    'default'          => 'auto',
-	    'options'          => array(
-	        'auto' => __( 'Auto', 'cmb2' ),
-	        'small'   => __( 'Small (40px)', 'cmb2' ),
-			'medium'   => __( 'Medium (80px)', 'cmb2' ),
-			'large'   => __( 'Large (120px)', 'cmb2' ),
-			'xlarge'   => __( 'X Large (200px)', 'cmb2' ),
-	    ),
-	));
-    // Module Spacing Selector
-	$module_setup->add_field( array(
-	    'name'             => 'Module Margin',
-	    'desc'             => 'Select the margin for the <b>top</b> and <b>bottom</b> of the module.',
-	    'id'               => $prefix . 'module_margin',
-	    'type'             => 'select',
-	    'show_option_none' => true,
-	    'options'          => array(
-			'small' => __( 'Small (40px)', 'cmb2' ),
-	        'medium'   => __( 'Medium (80px)', 'cmb2' ),
-            'large' => __( 'Large (120px)', 'cmb2' ),
-	    ),
-	));
-	 // Module Spacing Selector
-	$module_setup->add_field( array(
-	    'name'             => 'Module Text Color',
-	    'desc'             => 'Select the default text color for the module.<br />(You can overirde text colors using the editor styles above - this option is used to set the base color.)',
-	    'id'               => $prefix . 'module_text_color',
-	    'type'             => 'select',
-	    'show_option_none' => false,
-	    'default'          => 'black',
-	    'options'          => array(
-	        'black'   => __( 'Dark', 'cmb2' ),
-	        'white' => __( 'Light', 'cmb2' ),
-	    ),
-	));
-}
-
-// Module background metabox
-add_action( 'cmb2_init', 'module_metabox_background' );
-function module_metabox_background() {
-	// Start with an underscore to hide fields from custom fields list
-	$prefix = '_cmb_';
-	$module_background = new_cmb2_box( array(
-		'id'            => $prefix . 'module_background',
-		'title'         => __( 'Module Background', 'cmb2' ),
-		'object_types'  => array( 'module'), // Post type
-
-	));
-
-	// Module background color 1
-	$module_background->add_field( array(
-		'name' => 'Background Color',
-		'description' => 'Choose the background color of the module',
-		'id'   => $prefix . 'module_background_color',
-		'type' => 'colorpicker',
-	));
-	// Module background image
-	$module_background->add_field( array(
-		'name' => 'Background Image',
-		'description' => 'Use and image as the background<br />If image is added, it will be used as background and not the colors above<br />This image will also be used as the backup background for video backgrounds below',
-		'id'   => $prefix . 'module_background_image',
-		'type'  => 'file',
-	));
-	// Module Width Selector
-	$module_background->add_field( array(
-	    'name'             => 'Background Video Source',
-	    'desc'             => 'If you want to use a background video, select the source of the video ID.<br />This is required to source the correct video API for the ID above, if no source is choosen the video will not be shown - even with a supplied ID',
-	    'id'               => $prefix . 'module_background_video_source',
-	    'type'             => 'select',
-	    'show_option_none' => true,
-	    'options'          => array(
-	        'youtube' => __( 'YouTube', 'cmb2' ),
-	        'vimeo'   => __( 'Vimeo', 'cmb2' ),
-	    ),
-	));
-	// Module background image
-	$module_background->add_field( array(
-		'name' => 'Background Video ID',
-		'description' => 'Use the youtube or viemo video ID here.<br />Use the background image field above to set a fallback image for devices that do not support background videos (tablets and mobile)',
-		'id'   => $prefix . 'module_background_video',
-		'type'  => 'text',
-	));
-}
-
-// Module overlay metabox
-add_action( 'cmb2_init', 'module_metabox_overlay' );
-function module_metabox_overlay() {
-	// Start with an underscore to hide fields from custom fields list
-	$prefix = '_cmb_';
-	$module_overlay = new_cmb2_box( array(
-		'id'            => $prefix . 'module_overlay',
-		'title'         => __( 'Module Overlay', 'cmb2' ),
-		'object_types'  => array( 'module'), // Post type
-	));
-
-	// Module Overlay color 1
-	$module_overlay->add_field( array(
-		'name' => 'Overlay Color 1',
-		'description' => ' First color option (leave blank for default)',
-		'id'   => $prefix . 'module_overlay_color_one',
-		'type' => 'colorpicker',
-	));
-	// Module Overlay color 2
-	$module_overlay->add_field( array(
-		'name' => 'Overlay Color 2',
-		'description' => ' Second color to make gradient (if none - first color will be used as overlay)',
-		'id'   => $prefix . 'module_overlay_color_two',
-		'type' => 'colorpicker',
-	));
-	// Module Overlay Opacity
-	$module_overlay->add_field( array(
-	    'name'             => 'Overlay Opacity',
-	    'desc'             => 'Select the opacity of the overlay color/gradient (default is 50%)',
-	    'id'               => $prefix . 'module_overlay_opacity',
-	    'type'             => 'select',
-	    'show_option_none' => false,
-		'default'          => '5',
-	    'options'          => array(
-	        '99' => __( '100%', 'cmb2' ),
-	        '9'   => __( '90%', 'cmb2' ),
-			'8'   => __( '80%', 'cmb2' ),
-			'7'   => __( '70%', 'cmb2' ),
-			'6'   => __( '60%', 'cmb2' ),
-			'5'   => __( '50%', 'cmb2' ),
-			'4'   => __( '40%', 'cmb2' ),
-			'3'   => __( '30%', 'cmb2' ),
-			'2'   => __( '20%', 'cmb2' ),
-			'1'   => __( '10%', 'cmb2' ),
-	    ),
-	));
-	// Module Overlay direction
-	$module_overlay->add_field( array(
-	    'name'             => 'Overlay Direction',
-	    'desc'             => 'Select the overlay gradient direction. Gradient flows from color 1 to color 2',
-	    'id'               => $prefix . 'module_overlay_direction',
-	    'type'             => 'select',
-	    'show_option_none' => false,
-	    'default'          => 'right',
-	    'options'          => array(
-	        'right' => __( 'Right', 'cmb2' ),
-	        'left'   => __( 'Left', 'cmb2' ),
-			'top'   => __( 'Top', 'cmb2' ),
-			'bottom'   => __( 'Bottom', 'cmb2' ),
-	    ),
-	));
-}*/
-
-
-
-
-
-
 // Module Setup
 add_action("add_meta_boxes", "wp_content_module_setup");
 function wp_content_module_setup() {
@@ -447,9 +236,6 @@ function wp_content_module_setup() {
     </div>
 <?php }
 }
-
-
-
 // Module Background
 add_action("add_meta_boxes", "wp_content_module_overlay");
 function wp_content_module_overlay() {
@@ -545,11 +331,6 @@ function wp_content_module_overlay() {
         </div>
     <?php }
 }
-
-
-
-
-
 // Module Background
 add_action("add_meta_boxes", "wp_content_module_background");
 function wp_content_module_background() {
@@ -618,11 +399,6 @@ function wp_content_module_background() {
 <?php }
 
 }
-
-
-
-
-
 // Metabox input generation
 // Generate select dropdown
 function wp_content_module_select_input($name, $options, $value) {
@@ -640,12 +416,6 @@ function wp_content_module_select_input($name, $options, $value) {
 function wp_content_module_text_input($name, $class, $value) {
     echo '<input class="' . $class . '" type="text" name="' . $name . '"  value="' . $value . '" />';
 }
-
-
-
-
-
-
 // Save All Metadata
 add_action("save_post", "wp_content_module_meta_save", 10, 3);
 function wp_content_module_meta_save($post_id, $post, $update) {
@@ -672,15 +442,6 @@ function wp_content_module_meta_save($post_id, $post, $update) {
         }
     }
 }
-
-
-
-
-
-
-
-
-
 // Dispaly shortcode to copy
 add_action( 'add_meta_boxes', 'add_module_output' );
 function add_module_output() {
@@ -693,8 +454,6 @@ function add_module_output() {
     	echo '<p><i>Copy this shortcode and paste it into any content type editor (ex. post or page)</i></p>';
     }
 }
-
-
 
 /************************************************************************************
 *** Module Shortcode
@@ -730,30 +489,30 @@ function module_insert_func( $atts, $content = null ) {
             ${$key} = $value[0];
         }
 	    // Module classes
-	    $module_classes = array('wp-module', $_cmb_module_width);
+	    $module_classes = array('wp-module', $_module_width);
 	    // OUTPUT HTML BELOW
 	    ?>
 
-	    <div id="module-<?php the_ID(); ?>" class="module-margin--<?php echo isset($_cmb_module_margin) ? $_cmb_module_margin : ''; ?> module-text--<?php echo isset($_cmb_module_text_color) ? $_cmb_module_text_color : ''; ?>">
-    	    <div <?php post_class($module_classes); //WP Post Classes ?>>
-                <div class="module-wallpaper" style="background:<?php echo isset($_cmb_module_background_color) ? $_cmb_module_background_color : '#ffffff'; ?>; <?php echo isset($_cmb_module_background_image) ? 'background: url(' . $_cmb_module_background_image . ')' : ''; ?>"></div>
+        <div id="module-<?php the_ID(); ?>" class="module-margin--<?php echo isset($_module_margin) ? $_module_margin : ''; ?> module-text--<?php echo isset($_module_text_color) ? $_module_text_color : ''; ?>">
+            <div <?php post_class($module_classes); //WP Post Classes ?>>
+                <div class="module-wallpaper" style="background:<?php echo isset($_module_background_color) ? $_module_background_color : '#ffffff;'; ?>; <?php if(has_post_thumbnail()) { echo 'background: url('; the_post_thumbnail_url(); echo ')'; } ?>"></div>
 
                 <?php
-    	        // If Video
-    	        if(isset($_cmb_module_background_video_source))  { ?>
+                // If Video
+                if(isset($_module_background_video_source) ? $_module_background_video_source != '' : null )  { ?>
                     <div class="module-video">
-                        <div class="video <?php echo $_cmb_module_background_video_source; ?>" data-id="<?php echo $_cmb_module_background_video; ?>"></div>
+                        <div class="video <?php echo $_module_background_video_source; ?>" data-id="<?php echo $_module_video_id; ?>"></div>
                     </div>
                 <?php } ?>
 
-                <div class="module-overlay" style="background:#<?php echo isset($_cmb_module_overlay_color_one) ? $_cmb_module_overlay_color_one : ''; ?>; background:linear-gradient(to <?php echo isset($_cmb_module_overlay_direction) ? $_cmb_module_overlay_direction : ''; ?>, <?php echo isset($_cmb_module_overlay_color_one) ? $_cmb_module_overlay_color_one : ''; ?>, <?php echo isset($_cmb_module_overlay_color_two) ? $_cmb_module_overlay_color_two : ''; ?>); opacity:.<?php echo isset($_cmb_module_overlay_opacity) ? $_cmb_module_overlay_opacity : ''; ?>;"></div>
+                <div class="module-overlay" style="background:<?php echo isset($_module_overlay_color_1) ? $_module_overlay_color_1 : ''; ?>; background:linear-gradient(to <?php echo isset($_module_overlay_direction) ? $_module_overlay_direction : ''; ?>, <?php echo isset($_module_overlay_color_1) ? $_module_overlay_color_1 : ''; ?>, <?php echo isset($_module_overlay_color_2) ? $_module_overlay_color_2 : ''; ?>); opacity:.<?php echo isset($_module_overlay_opacity) ? $_module_overlay_opacity : ''; ?>;"></div>
 
-    	        <div class="module-content <?php echo 'module-content--height-' . $_cmb_module_height . ' module-content--width-' . $_cmb_module_content_width; ?> ">
-    	            <?php the_content(); ?>
-    	        </div>
+                <div class="module-content <?php echo 'module-content--height-' . $_module_padding . ' module-content--width-' . $_module_content_width; ?> ">
+                    <?php the_content(); ?>
+                </div>
 
-    	    </div>
-	    </div>
+            </div>
+        </div>
 
 		<?php
 		// Reset Query
@@ -766,7 +525,6 @@ function module_insert_func( $atts, $content = null ) {
 		return $content_module;
 	}
 }
-
 
 /************************************************************************************
 *** Javascript
@@ -852,7 +610,6 @@ function load_module_scripts() {
 
 <?php } }
 
-
 /************************************************************************************
 *** Single View (Preview)
 	Set custom single view template for Module post type.
@@ -880,4 +637,5 @@ function module_preview_template($single_template) {
 register_rest_field( 'module', 'metadata', array(
     'get_callback' => function ( $data ) {
         return get_post_meta( $data['id'], '', '' );
-    }, ));
+    }
+));
